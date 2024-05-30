76007db7f52ba67024f9a2a6ca51c28583265e9b
Arthur <arthurunsong01@gmail.com>
2024-05-30 23:41:44 +0800

    update beberapa file

--- static/about.php
+++ static/about.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <title>Article Cards</title>
+    <link rel="stylesheet" href="../css/about.css">
 </head>
 <body>
-<header>
+    <header>
         <div class="container">
             <div class="logo">
                 <h1>FactPedia</h1>
@@ -23,12 +23,52 @@
         </div>
     </header>
     <main>
-        <p>Informasi tentang anggota-anggota kelompok.</p>
-        <ul>
-            <li>Anggota 1: Nama, NIM, dll.</li>
-            <li>Anggota 2: Nama, NIM, dll.</li>
-            <!-- Tambahkan informasi anggota lainnya -->
-        </ul>
+        <div class="container article-grid">
+            <div class="article-card">
+                <div class="article-image">
+                    <img src="../img/heski.jpg" alt="picture">
+                </div>
+                <div class="article-content">
+                    <p class="article-date">04 Februari 2024 / Sport</p>
+                    <h2 class="article-title">The Reds, The Biggest Club In England!</h2>
+                    <p class="article-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident ullam quis cum quas numquam! Molestias...</p>
+                    <a href="#" class="read-more">Read More</a>
+                </div>
+            </div>
+            <div class="article-card">
+                <div class="article-image">
+                    <img src="../img/Arthur.jpg" alt="Picture">
+                </div>
+                <div class="article-content">
+                    <p class="article-date">05 Februari 2024 / Sport</p>
+                    <h2 class="article-title">Another Great Club!</h2>
+                    <p class="article-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident ullam quis cum quas numquam! Molestias...</p>
+                    <a href="#" class="read-more">Read More</a>
+                </div>
+            </div>
+            <div class="article-card">
+                <div class="article-image">
+                    <img src="../img/joy.jpg" alt="Yet Another Logo">
+                </div>
+                <div class="article-content">
+                    <p class="article-date">06 Februari 2024 / Sport</p>
+                    <h2 class="article-title">Yet Another Great Club!</h2>
+                    <p class="article-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident ullam quis cum quas numquam! Molestias...</p>
+                    <a href="#" class="read-more">Read More</a>
+                </div>
+            </div>
+            <div class="article-card">
+                <div class="article-image">
+                    <img src="../img/irham.jpg" alt="And Another Logo">
+                </div>
+                <div class="article-content">
+                    <p class="article-date">07 Februari 2024 / Sport</p>
+                    <h2 class="article-title">And Another Great Club!</h2>
+                    <p class="article-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident ullam quis cum quas numquam! Molestias...</p>
+                    <a href="#" class="read-more">Read More</a>
+                </div>
+            </div>
+        </div>
     </main>
     <footer>
         <p>&copy; 2024 Kelompok Kami. All rights reserved.</p>
