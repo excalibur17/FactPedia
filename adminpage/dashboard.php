66735bc8a0284e8b8e6fe95afbb46960bc096636
heskipranata <heskipranata14@gmail.com>
2024-06-01 00:16:22 +0800

    Mengintegrasikan dashboard admin dengan db

--- adminpage/dashboard.php
+++ adminpage/dashboard.php
@@ -1,3 +1,6 @@
+<?php
+require "./function/func.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -39,38 +42,57 @@
             <div class="posts">
                 <section class="stats">
                     <div class="stat-box">
-                        <h3>Total Posts</h3>
-                        <p>120</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+                        </svg>
+                        <div class="stat-info">
+                            <p><?php echo $total_users ?></p>
+                            <h5>Total Users</h5>
+                        </div>
                     </div>
                     <div class="stat-box">
-                        <h3>Total Users</h3>
-                        <p>75</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path d="M168 80c-13.3 0-24 10.7-24 24V408c0 8.4-1.4 16.5-4.1 24H440c13.3 0 24-10.7 24-24V104c0-13.3-10.7-24-24-24H168zM72 480c-39.8 0-72-32.2-72-72V112C0 98.7 10.7 88 24 88s24 10.7 24 24V408c0 13.3 10.7 24 24 24s24-10.7 24-24V104c0-39.8 32.2-72 72-72H440c39.8 0 72 32.2 72 72V408c0 39.8-32.2 72-72 72H72zM176 136c0-13.3 10.7-24 24-24h96c13.3 0 24 10.7 24 24v80c0 13.3-10.7 24-24 24H200c-13.3 0-24-10.7-24-24V136zm200-24h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H376c-13.3 0-24-10.7-24-24s10.7-24 24-24zM200 272H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24zm0 80H408c13.3 0 24 10.7 24 24s-10.7 24-24 24H200c-13.3 0-24-10.7-24-24s10.7-24 24-24z" />
+                        </svg>
+                        <div class="stat-info">
+                            <p><?php echo $total_posts ?></p>
+                            <h5>Total Posts</h5>
+                        </div>
                     </div>
                 </section>
                 <section class="recent-posts">
                     <h3>Recent Posts</h3>
                     <table>
                         <thead>
-                            <tr>
+                            <tr class="tr-center">
+                                <th>No.</th>
                                 <th>Title</th>
                                 <th>Author</th>
+                                <th>Categori</th>
                                 <th>Date</th>
                                 <th>Status</th>
+                                <th>View</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Article 1</td>
-                                <td>Author 1</td>
-                                <td>2024-05-30</td>
-                                <td>Published</td>
-                            </tr>
-                            <tr>
-                                <td>Article 2</td>
-                                <td>Author 2</td>
-                                <td>2024-05-29</td>
-                                <td>Draft</td>
-                            </tr>
+                            <?php $i = 1; ?>
+                            <?php foreach ($data as $row) : ?>
+                                <tr class="text-l">
+                                    <td><?= $i++; ?></td>
+                                    <td><?= $row['title']; ?></td>
+                                    <td><?= $row['author']; ?></td>
+                                    <td><?= ucwords($row['category']); ?></td>
+                                    <td><?= $row['created_at']; ?></td>
+                                    <td><?= ucwords($row['status']); ?></td>
+                                    <td class="row-view">
+                                        <button class="btn-view">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                                <path fill="#0f51c2" d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z" />
+                                            </svg>
+                                        </button>
+                                    </td>
+                                </tr>
+                            <?php endforeach ?>
                         </tbody>
                     </table>
                 </section>
--- adminpage/function/func.php
+++ adminpage/function/func.php
@@ -8,3 +8,40 @@ $result_adm = $conn->query($sql);
 // Mengambil data dari users
 $sqll = "SELECT * FROM users";
 $get_user = $conn->query($sqll);
+
+//Mengambil data dari db dan akan ditampilkan di dashboard
+// Mengambil data dari db dan akan ditampilkan di dashboard
+$show = "SELECT * FROM trivias";
+$get_data = $conn->query($show);
+
+// Periksa apakah query berhasil dan mengembalikan data
+if ($get_data->num_rows > 0) {
+    // Mengambil semua data sebagai array
+    $data = $get_data->fetch_all(MYSQLI_ASSOC);
+} else {
+    $data = [];
+}
+
+// Mengambil total  user dari database
+$sql_total_users = "SELECT COUNT(*) AS total_users FROM users"; 
+$result_total_users = $conn->query($sql_total_users);
+
+if ($result_total_users->num_rows > 0) {
+    $row_total_users = $result_total_users->fetch_assoc();
+    $total_users = $row_total_users["total_users"];
+} else {
+    $total_users = 0; 
+}
+
+// Mengambil total trivia dari database
+$sql_tot_post = "SELECT COUNT(*) AS total_posts FROM trivias";
+$result_tot_post = $conn->query($sql_tot_post);
+
+if ($result_tot_post->num_rows > 0) {
+    $row = $result_tot_post->fetch_assoc();
+    $total_posts = $row["total_posts"];
+} else {
+    $total_posts = 0; 
+}
+
+
